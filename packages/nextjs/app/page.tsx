63a82c6d1ebd9a2aea601ec8a737ed631131016c
Austin Griffith <austin@ethereum.org>
2024-08-07 11:01:23 -0600

    really gross way to generate smoothed land tiles onchain

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,66 +1,72 @@
 "use client";
 
-import Link from "next/link";
+import { useEffect, useState } from "react";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import Grid from "~~/components/Grid";
 import { Address } from "~~/components/scaffold-eth";
+import { useScaffoldReadContract, useScaffoldWriteContract } from "~~/hooks/scaffold-eth";
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
+  const [lastTokenIds, setLastTokenIds] = useState<number[]>([]);
+
+  const { data: balance } = useScaffoldReadContract({
+    contractName: "GridNFT",
+    functionName: "balanceOf",
+    args: [connectedAddress],
+  });
+
+  const { data: totalSupply } = useScaffoldReadContract({
+    contractName: "GridNFT",
+    functionName: "totalSupply",
+  });
+
+  const { writeContractAsync: createGrid } = useScaffoldWriteContract("GridNFT");
+
+  useEffect(() => {
+    if (totalSupply) {
+      const totalSupplyNum = Number(totalSupply); // Convert BigInt to Number
+      const last10 = [];
+      for (let i = Math.max(1, totalSupplyNum - 9); i <= totalSupplyNum; i++) {
+        last10.push(i);
+      }
+      setLastTokenIds(last10.reverse());
+    }
+  }, [totalSupply]);
 
   return (
     <>
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5">
-          <h1 className="text-center">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
-          </h1>
-          <div className="flex justify-center items-center space-x-2 flex-col sm:flex-row">
-            <p className="my-2 font-medium">Connected Address:</p>
-            <Address address={connectedAddress} />
-          </div>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
+          <p>Total Supply: {totalSupply ? totalSupply.toString() : "0"}</p>
         </div>
 
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contracts
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
+        <div className="pt-5"></div>
+        <div className="px-5">
+          <Address address={connectedAddress} />
+        </div>
+
+        <div className="px-5">
+          <p>Your Grid Balance: {balance ? balance.toString() : "0"}</p>
+        </div>
+
+        <div className="pb-10">
+          <button
+            onClick={() => createGrid({ functionName: "createGrid" })}
+            className="mt-5 btn btn-primary font-bold py-2 px-4 rounded"
+          >
+            Create Grid
+          </button>
+        </div>
+
+        <div className="overflow-x-auto w-full">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center">
+            {lastTokenIds.map(tokenId => (
+              <div key={tokenId} className="flex justify-center">
+                <Grid tokenId={tokenId} />
+              </div>
+            ))}
           </div>
         </div>
       </div>
